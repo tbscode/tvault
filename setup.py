e647db836aeef518dfa8a4236dbbaef0cffa63f3
Tim Schupp <tim@timschupp.de>
2024-06-24 01:53:39 +0200

    setup better

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "tvault=tvault.main:main",
+            "tvault=tvault.vault:main",
         ],
     },
     classifiers=[
