54dbc0b1bc667ef094a6850db33d099d821c199c
Tim Schupp <tim@timschupp.de>
2024-06-24 02:00:16 +0200

    self install script

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# wget https://raw.githubusercontent.com/tbscode/tvault/main/install.sh -O - | bash
+
+mkdir -p /home/$USER/.config/tvault
+cd /home/$USER/.config/tvault
+python3 -m venv venv
+source venv/bin/activate
+pip install git+https://github.com/tbscode/tvault
+
+echo "alias tvault='/home/$USER/.config/tvault/venv/bin/tvault'" >> /home/$USER/.bashrc
