3d409281868ec73fe7cc9bad9f2b763a3bf5429e
Tim Schupp <tim@timschupp.de>
2024-06-24 02:22:20 +0200

    self update

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 mkdir -p /home/$USER/.config/tvault
 cd /home/$USER/.config/tvault
+if [ -d "venv" ]; then
+    rm -rf venv
+fi
 python3 -m venv venv
 source venv/bin/activate
 pip install git+https://github.com/tbscode/tvault
